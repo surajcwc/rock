c0f1bd8b4ba9c3eb446cfc73d86f3c47d74a7056
suraj <suraj.kumar@cwctechnologies.in>
2025-09-09 12:22:42 +0530

    hell

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 
 import './App.css'
-import Navbar from './Navbar.jsx'
+import Navbar from './navbar.jsx'
 import Hero from './hero.jsx'
 import Section from './Section.jsx'
-import Banner from './Banner.jsx'
+import Banner from './banner.jsx'
 function App() {
 
   return (
